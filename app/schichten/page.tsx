b963c8e4aa715b8088de5016dd5db5cae6d60d52
Lippi1975 <franklippert@gmx.de>
2026-08-20 16:44:34 +0200

    Create page.tsx

---
+++ app/schichten/page.tsx
@@ -0,0 +1,10 @@
+export default function Schichten() {
+  return (
+    <main className="container">
+      <div className="card">
+        <h1>Schichten</h1>
+        <p>Hier werden Schichten verwaltet.</p>
+      </div>
+    </main>
+  );
+}
